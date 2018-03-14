027e607f5ea63e56d21698f40fa41a53dc325405
Juan Daniel Morales <moralesarias94@gmail.com>
2018-03-14 15:36:22 -0500

    tests

---
+++ test.py
@@ -0,0 +1,15 @@
+
+import unittest
+import requests
+import json
+
+class TestRemoteItems(unittest.TestCase):
+    def testGetLatestItems(self):
+        items = [{"_id": "12345", "nombre": "Leche"}]
+        url = 'https://easycompras-api.herokuapp.com/productos'
+        params = {'where' : json.dumps({'nombre': 'Leche'})}
+        producto = requests.get(url, params=params).json()['_items'][0]
+        self.assertEqual(items[0]['nombre'], producto['nombre'])
+
+if __name__ == '__main__':
+    unittest.main()
